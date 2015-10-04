0a4e004c0ae52d7157cb7df3c44133e26ddffb37
Rob Spectre <rob@brooklynhacker.com>
2015-10-04 15:53:08 +0100

    1) Stubbed out scoreboard. 2) Stubbed out training corpus. 3) Stubbed out queue for classification. 4) Stubbed out queue for trainers.

---
+++ err-social-support/__init__.py
@@ -0,0 +1,180 @@
+import operator
+
+from errbot import BotPlugin
+from errbot import botcmd
+
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Cursor
+
+from zdesk import Zendesk
+
+
+class SocialSupport(BotPlugin):
+    """An err plugin for monitoring social channels by
+       support organizations."""
+    min_err_version = '3.0.0'
+
+    def get_configuration_template(self):
+        return {'TWITTER_CONSUMER_KEY': 'aaaaa',
+                'TWITTER_CONSUMER_SECRET': 'bbbb',
+                'TWITTER_OAUTH_TOKEN': 'ccccc',
+                'TWITTER_OAUTH_SECRET': 'dddd',
+                'TWITTER_SEARCH_QUERY': 'adcap',
+                'ZENDESK_INSTANCE_URI': 'https://example.zendesk.com',
+                'ZENDESK_USER': 'example@example.com',
+                'ZENDESK_PASSWORD': 'derppassword'}
+
+    def check_configuration(self, configuration):
+        if configuration is not None:
+            for key, value in self.get_configuration_template().items():
+                if not configuration.get(key, None):
+                    self.log.info("Configuration is missing required "
+                                  "item: {0}".format(key))
+                    return
+
+            super(SocialSupport, self).check_configuration(configuration)
+
+        return
+
+    def activate(self):
+        if self.config is None:
+            self.log.info("SocialSupport is not configured - plugin not "
+                          "activating.")
+        else:
+            # Configure clients
+            auth = OAuthHandler(self.config['TWITTER_CONSUMER_KEY'],
+                                self.config['TWITTER_CONSUMER_SECRET'])
+            auth.set_access_token(self.config['TWITTER_OAUTH_TOKEN'],
+                                  self.config['TWITTER_OAUTH_SECRET'])
+            self.twitter_client = API(auth)
+            self.since_id = None
+
+            self.zendesk_client = Zendesk(self.config['ZENDESK_INSTANCE_URI'],
+                                          self.config['ZENDESK_USER'],
+                                          self.config['ZENDESK_PASSWORD'])
+
+            self.log.info("Starting SocialSupport.")
+            super(SocialSupport, self).activate()
+
+            # Set up persistence.
+            self.log.info("Setting up persistence.")
+            self['SUPPORT_TRAINING_CORPUS'] = []
+            self['SUPPORT_TRAINING_QUEUE'] = []
+            self['SUPPORT_TRAINER_QUEUE'] = {}
+            self['TRAINER_SCOREBOARD'] = {}
+
+    def search_tweets(self, limit=100, **kwargs):
+        tweets = []
+
+        for tweet in Cursor(self.twitter_client.search, **kwargs).items(limit):
+            self.since_id = tweet.id
+            tweets.append(tweet)
+
+        return tweets
+
+    def fetch_tweets(self, limit=100, since_id=None, **kwargs):
+        return self.search_tweets(q=self.config['TWITTER_SEARCH_QUERY'],
+                                  limit=limit,
+                                  since_id=since_id,
+                                  **kwargs)
+
+    def load_tweets_into_queue(self, queue):
+        self.log.info("Loading tweets into {0}.".format(queue))
+        if not self[queue]:
+            self.log.info("No tweets found in {0}".format(queue))
+            tweets = self.fetch_tweets()
+            self[queue] = tweets
+            self.log.info("Added {0} tweets to {1}".format(len(self[queue]),
+                                                           queue))
+        else:
+            temp_queue = self[queue]
+            self.log.info("{0} tweets found in {1}.".format(len(temp_queue),
+                                                            queue))
+            tweets = self.fetch_tweets()
+            for tweet in tweets:
+                self.log.debug("Adding tweet {0}.".format(tweet.id))
+                temp_queue.append(tweet)
+            self[queue] = temp_queue
+            self.log.info("{0} tweets added to {1}.".format(len(tweets),
+                                                            queue))
+
+        return self[queue]
+
+    def pop_tweet_from_queue(self, queue):
+        temp_queue = self[queue]
+        tweet = temp_queue.pop().text
+        self[queue] = temp_queue
+
+        return tweet
+
+    def assign_tweet_to_trainer(self, trainer, tweet):
+        temp_trainers = self['SUPPORT_TRAINER_QUEUE']
+        temp_trainers[trainer] = tweet
+        self['SUPPORT_TRAINER_QUEUE'] = temp_trainers
+        return self['SUPPORT_TRAINER_QUEUE']
+
+    def update_corpus(self, corpus, tweet, classification):
+        temp_corpus = self[corpus]
+        temp_corpus.append((tweet, classification))
+        self[corpus] = temp_corpus
+        return self[corpus]
+
+    def update_trainer_scoreboard(self, trainer, points=1):
+        temp_scoreboard = self['TRAINER_SCOREBOARD']
+        if temp_scoreboard.get(trainer, None):
+            temp_scoreboard[trainer] += points
+        else:
+            temp_scoreboard[trainer] = points
+
+        self['TRAINER_SCOREBOARD'] = temp_scoreboard
+        return self['TRAINER_SCOREBOARD']
+
+    def retrieve_top_trainers(self, limit=5):
+        top_five = sorted(self['TRAINER_SCOREBOARD'].items(),
+                          key=operator.itemgetter(1),
+                          reverse=True)[:limit]
+        return top_five
+
+    @botcmd
+    def train_status(self, message, args):
+        if not self['SUPPORT_TRAINING_CORPUS']:
+            yield "No tweets found in training corpus. Use !train to " \
+                  "receive a tweet to classify."
+        else:
+            corpus_count = len(self['SUPPORT_TRAINING_CORPUS'])
+            yield "Training corpus size: {0}".format(str(corpus_count))
+
+        if not self['SUPPORT_TRAINING_QUEUE']:
+            yield "No tweets awaiting classification - fetching more " \
+                  "to classify."
+            self.load_tweets_into_queue('SUPPORT_TRAINING_QUEUE')
+        else:
+            queue_count = len(self['SUPPORT_TRAINING_QUEUE'])
+            yield "Tweets awaiting classification: {0}".format(str(queue_count))
+
+        if self['TRAINER_SCOREBOARD']:
+            yield "Top 5 trainers:"
+
+            top_five = self.retrieve_top_trainers()
+
+            counter = 1
+            for trainer in top_five:
+                yield "{0}: {1} - {2}".format(str(counter),
+                                              trainer[0],
+                                              str(trainer[1]))
+                counter += 1
+
+    @botcmd
+    def train_gimme(self, message, args):
+        if not self['SUPPORT_TRAINING_QUEUE']:
+            yield "No tweets awaiting classification - fetching more " \
+                  "to classify. Try !train gimme again to get a tweet to " \
+                  "classify."
+            self.load_tweets_into_queue('SUPPORT_TRAINING_QUEUE')
+        else:
+            tweet = self.pop_tweet_from_queue('SUPPORT_TRAINING_QUEUE')
+            self.assign_tweet_to_trainer(message.frm.person, tweet)
+            yield "Does the person submitting this tweet need technical " \
+                  "support? Respond with !train yes or !train no."
+            yield tweet
---
+++ tests/test_err-social-support.py
@@ -0,0 +1,219 @@
+import logging
+
+from vcr import VCR
+
+from errbot.backends.test import FullStackTest
+
+config = {'TWITTER_CONSUMER_KEY': 'aaaaa',
+          'TWITTER_CONSUMER_SECRET': 'bbbb',
+          'TWITTER_OAUTH_TOKEN': 'ccccc',
+          'TWITTER_OAUTH_SECRET': 'dddd',
+          'TWITTER_SEARCH_QUERY': 'usopen',
+          'ZENDESK_INSTANCE_URI': 'https://example.zendesk.com',
+          'ZENDESK_USER': 'example@example.com',
+          'ZENDESK_PASSWORD': 'derppassword'}
+
+"""
+config = {'TWITTER_CONSUMER_KEY': 'CX26EI7n7Ud7E0DruTuC2E4DT',
+          'TWITTER_CONSUMER_SECRET':
+          'brnx6QJjsMM89wFD9v1XmLNvUeqL4Iu6nCyANoZvGSYq6dGouy',
+          'TWITTER_OAUTH_TOKEN':
+          '10947242-mZRf6GQv0W30eOovmcuBHOAoc1KlDWUTWK5OBd88p',
+          'TWITTER_OAUTH_SECRET': '1UDcbwGFDOuGXeK0fTqGxgYEMYqMFveg3EmqBohpwHOaL',
+          'TWITTER_SEARCH_QUERY': 'usopen',
+          'ZENDESK_INSTANCE_URI': 'https://example.zendesk.com',
+          'ZENDESK_USER': 'example@example.com',
+          'ZENDESK_PASSWORD': 'derppassword'}
+"""
+
+vcr = VCR(serializer='json',
+          cassette_library_dir='tests/fixtures/cassettes',
+          record_mode='once',
+          filter_headers=['authorization'])
+
+
+class TestSocialSupportWithoutConfiguration(FullStackTest):
+    def setUp(self, extra_plugin_dir='.', logging_level=logging.INFO):
+        super(TestSocialSupportWithoutConfiguration,
+              self).setUp(extra_plugin_dir=extra_plugin_dir,
+                          loglevel=logging_level)
+
+    def test_no_configuration(self):
+        plugin_list = self.bot.get_all_active_plugin_names()
+        self.assertFalse("SocialSupport" in plugin_list)
+
+    def test_wrong_configuration(self):
+        self.bot.deactivate_plugin_by_name('SocialSupport')
+        incomplete_configuration = {"TWITTER_CONSUMER_KEY": "xxxx"}
+        self.bot.set_plugin_configuration('SocialSupport',
+                                          incomplete_configuration)
+
+        self.bot.activate_plugin('SocialSupport')
+
+        plugin_list = self.bot.get_all_active_plugin_names()
+        self.assertFalse("SocialSupport" in plugin_list)
+
+
+class TestSocialSupport(FullStackTest):
+    def setUp(self):
+        super(TestSocialSupport, self).setUp(extra_plugin_dir='.',
+                                             loglevel=logging.INFO)
+
+        self.bot.set_plugin_configuration('SocialSupport', config)
+        self.bot.activate_plugin('SocialSupport')
+
+        self.plugin = self.bot.get_plugin_obj_by_name('SocialSupport')
+
+    def assertCommand(self, command, response, **kwargs):
+        self.bot.push_message(command)
+        self.assertIn(response, self.bot.pop_message(**kwargs),
+                      "Did not find expected response to command "
+                      "{0}: {1}".format(command, response))
+
+
+class TestSocialSupportUtilities(TestSocialSupport):
+    @vcr.use_cassette('tweets.json', inject_cassette=True)
+    def test_search_tweets(self, cassette):
+        tweets = self.plugin.search_tweets(q=config['TWITTER_SEARCH_QUERY'])
+        self.assertTrue(len(tweets) == 100)
+        self.assertEquals(tweets[0].id, 640303466536148992)
+        self.assertEquals(cassette.play_count, 7)
+
+    @vcr.use_cassette('tweets.json', inject_cassette=True)
+    def test_fetch_tweets(self, cassette):
+        tweets = self.plugin.fetch_tweets()
+        self.assertTrue(len(tweets) == 100)
+        self.assertEquals(tweets[0].id, 640303466536148992)
+        self.assertEquals(cassette.play_count, 7)
+
+    @vcr.use_cassette('tweets.json')
+    def test_load_tweets_into_queue_empty(self):
+        self.plugin.load_tweets_into_queue("SUPPORT_TRAINING_QUEUE")
+        self.assertEquals(len(self.plugin["SUPPORT_TRAINING_QUEUE"]), 100)
+
+    @vcr.use_cassette('tweets.json')
+    def test_load_tweets_into_queue_not_empty(self):
+        self.plugin["SUPPORT_TRAINING_QUEUE"] = ["Item"]
+        self.plugin.load_tweets_into_queue("SUPPORT_TRAINING_QUEUE")
+        self.assertEquals(len(self.plugin["SUPPORT_TRAINING_QUEUE"]), 101)
+
+    @vcr.use_cassette('tweets.json')
+    def test_pop_tweet_from_queue(self):
+        self.plugin.load_tweets_into_queue("SUPPORT_TRAINING_QUEUE")
+        tweet = self.plugin.pop_tweet_from_queue("SUPPORT_TRAINING_QUEUE")
+        self.assertIn("Good luck to freshman Francesca Di Lorenzo", tweet)
+        self.assertEquals(len(self.plugin["SUPPORT_TRAINING_QUEUE"]),
+                          99)
+
+    @vcr.use_cassette('tweets.json')
+    def test_assign_tweet_to_trainer(self):
+        self.plugin.load_tweets_into_queue("SUPPORT_TRAINING_QUEUE")
+        tweet = self.plugin.pop_tweet_from_queue("SUPPORT_TRAINING_QUEUE")
+        test = self.plugin.assign_tweet_to_trainer("Tester",
+                                                   tweet)
+        tweet_text = "RT @OhioState_WTEN: Good luck to freshman Francesca " \
+                     "Di Lorenzo who opens play in @usopen juniors Sunday! " \
+                     "http://t.co/b3a2m6S2BN #GoBucks ht…"
+        self.assertEquals({"Tester": tweet_text}, test)
+
+    def test_update_corpus(self):
+        self.plugin.update_corpus('SUPPORT_TRAINING_CORPUS',
+                                  'I need help!',
+                                  'pos')
+        self.assertEquals([('I need help!', 'pos')],
+                         self.plugin['SUPPORT_TRAINING_CORPUS'])
+
+    def test_update_trainer_scoreboard_trainer_not_exist(self):
+        self.plugin.update_trainer_scoreboard('Tester', 1)
+        self.assertEquals({'Tester': 1},
+                          self.plugin['TRAINER_SCOREBOARD'])
+
+    def test_update_trainer_scoreboard_trainer_exist(self):
+        self.plugin.update_trainer_scoreboard('Tester', 1)
+        self.plugin.update_trainer_scoreboard('Tester', 2)
+        self.assertEquals({'Tester': 3},
+                          self.plugin['TRAINER_SCOREBOARD'])
+
+    def test_retrieve_top_trainers(self):
+        self.plugin.update_trainer_scoreboard('One', 1)
+        self.plugin.update_trainer_scoreboard('Two', 2)
+        self.plugin.update_trainer_scoreboard('Three', 3)
+        self.plugin.update_trainer_scoreboard('Four', 4)
+        self.plugin.update_trainer_scoreboard('Five', 5)
+        self.plugin.update_trainer_scoreboard('Six', 6)
+
+        test = self.plugin.retrieve_top_trainers()
+        self.assertEquals(len(test), 5)
+        self.assertEquals(('Six', 6), test[0])
+
+    def test_retrieve_top_trainers_less_than_limit(self):
+        self.plugin.update_trainer_scoreboard('One', 1)
+        self.plugin.update_trainer_scoreboard('Two', 2)
+        self.plugin.update_trainer_scoreboard('Three', 3)
+
+        test = self.plugin.retrieve_top_trainers()
+        self.assertEquals(len(test), 3)
+
+
+class TestSocialSupportCommands(TestSocialSupport):
+    @vcr.use_cassette('tweets.json')
+    def test_train_status_uninitialized(self):
+        self.bot.push_message("!train status")
+        self.assertIn("No tweets found in training corpus",
+                      self.bot.pop_message())
+        self.assertIn("No tweets awaiting classification",
+                      self.bot.pop_message())
+
+    @vcr.use_cassette('tweets.json')
+    def test_train_status_no_corpus(self):
+        self.plugin.load_tweets_into_queue("SUPPORT_TRAINING_QUEUE")
+        self.bot.push_message("!train status")
+        self.assertIn("No tweets found in training corpus",
+                      self.bot.pop_message())
+        self.assertIn("Tweets awaiting classification: 100",
+                      self.bot.pop_message())
+
+    @vcr.use_cassette('tweets.json')
+    def test_train_status(self):
+        self.plugin.load_tweets_into_queue("SUPPORT_TRAINING_QUEUE")
+        self.plugin.update_corpus('SUPPORT_TRAINING_CORPUS',
+                                  'I need help!',
+                                  'pos')
+
+        self.plugin.update_trainer_scoreboard('One', 1)
+        self.plugin.update_trainer_scoreboard('Two', 2)
+        self.plugin.update_trainer_scoreboard('Three', 3)
+        self.plugin.update_trainer_scoreboard('Four', 4)
+        self.plugin.update_trainer_scoreboard('Five', 5)
+        self.plugin.update_trainer_scoreboard('Six', 6)
+
+        self.bot.push_message("!train status")
+        self.assertIn("Training corpus size: 1",
+                      self.bot.pop_message())
+        self.assertIn("Tweets awaiting classification: 100",
+                      self.bot.pop_message())
+        self.assertEquals("Top 5 trainers:", self.bot.pop_message())
+        self.assertEquals("1: Six - 6", self.bot.pop_message())
+        self.assertEquals("2: Five - 5", self.bot.pop_message())
+        self.assertEquals("3: Four - 4", self.bot.pop_message())
+        self.assertEquals("4: Three - 3", self.bot.pop_message())
+        self.assertEquals("5: Two - 2", self.bot.pop_message())
+
+    @vcr.use_cassette('tweets.json', inject_cassette=True)
+    def test_train_gimme(self, cassette):
+        self.plugin.load_tweets_into_queue("SUPPORT_TRAINING_QUEUE")
+        self.bot.push_message("!train gimme")
+        self.assertIn("Does the person submitting this tweet",
+                      self.bot.pop_message())
+        self.assertIn("Good luck to freshman Francesca Di Lorenzo",
+                      self.bot.pop_message())
+        tweet_text = "RT @OhioState_WTEN: Good luck to freshman Francesca " \
+                     "Di Lorenzo who opens play in @usopen juniors Sunday! " \
+                     "http://t.co/b3a2m6S2BN #GoBucks ht…"
+        self.assertEquals({"gbin@localhost": tweet_text},
+                          self.plugin['SUPPORT_TRAINER_QUEUE'])
+
+    @vcr.use_cassette('tweets.json', inject_cassette=True)
+    def test_train_gimme_empty_queue(self, cassette):
+        self.assertCommand("!train gimme",
+                           "No tweets awaiting classification")
